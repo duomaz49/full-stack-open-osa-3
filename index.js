af7ffa1df9bf9c49cece167c09174b6eb2dbec8b
Tomaatti <tuomas49@gmail.com>
2025-03-03 22:08:49 +0900

    tehtävät 3.7 - 3.8

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const generateId = require("./helper");
+const morgan = require('morgan');
 const app = express();
+
+morgan.token('body', (req) => JSON.stringify(req.body));
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
 
 app.use(express.json());
 
