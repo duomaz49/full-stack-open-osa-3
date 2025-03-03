ab6bf5d71bd16a3fc30a158a8cd04214b001fe21
Tomaatti <tuomas49@gmail.com>
2025-03-03 21:52:46 +0900

    tehtävät 3.1-3.6

---
+++ helper.js
@@ -0,0 +1,7 @@
+const generateId = (persons) => {
+  const maxId =
+    persons.length > 0 ? Math.max(...persons.map((n) => Number(n.id))) : 0;
+  return String(maxId + 1);
+};
+
+module.exports = generateId;
